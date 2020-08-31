0c1de322fd9913514f5663c899364133acb6092b
Alex <u16918@yandex.ru>
2020-08-31 21:10:29 +0700

    HomeWork

--- src/ru/progwards/java1/lessons/interfaces/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -4,16 +4,15 @@ import java.util.Arrays;
 
 public class ArraySort {
     public static <compareWeight> void sort(compareWeight[] a) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = i+1; j < a.length - i - 1;j++) {
-                if (a[j] != a[j+1]) {
-                    compareWeight m = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = m;
+        for (int i = 0; i < a.length - 1; i++)
+            for (int j = 0; j < a.length - i - 1; j++)
+                if (!a[j].equals(a[j + 1])) {
+                    compareWeight temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
-            }
         }
-    }
+
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new Animal[]{new Animal(200.5), new Animal(10.3), new Animal(300.0)}));
