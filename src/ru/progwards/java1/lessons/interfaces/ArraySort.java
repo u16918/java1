993e69714906dd2fdf356005ca191adb1fdd522e
Alex <u16918@yandex.ru>
2020-09-10 22:58:02 +0700

    HomeWork

--- src/ru/progwards/java1/lessons/interfaces/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -7,13 +7,13 @@ import static ru.progwards.java1.lessons.interfaces.CompareWeight.CompareResult.
 public class ArraySort {
 
     public static void sort(CompareWeight[] a) {
-        CompareWeight a1;
+        CompareWeight resalt;
         for (int i=0; i<a.length; i++) {
             for (int j=i+1; j<a.length; j++) {
                 if (a[i].compareWeight(a[j]) == CompareWeight.CompareResult.GREATER) {
-                    a1 = a[i];
+                    resalt = a[i];
                     a[i] = a[j];
-                    a[j] = a1;
+                    a[j] = resalt;
                 }
             }
         }
